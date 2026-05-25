11753561d2e452364b25e89c2d0ffb9cac2eb3bc
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-05-25 11:10:24 -0400

    refactorizacion fibonacci

--- component.ts
+++ component.ts
@@ -1,23 +1,22 @@
 // Función que genera los primeros 'n' números de Fibonacci
 function generarFibonacci(n: number): number[] {
-    const serie: number[] = [0, 1];
+    const secuencia: number[] = [0, 1];
 
     if (n <= 0) return [];
     if (n === 1) return [0];
 
     for (let i = 2; i < n; i++) {
-        const siguienteNumero = serie[i - 1] + serie[i - 2];
-        serie.push(siguienteNumero);
+        secuencia.push(secuencia[i - 1] + secuencia[i - 2]);
     }
 
-    return serie;
+    return secuencia;
 }
 
 // Configura cuántos números quieres mostrar
-const cantidadNumeros: number = 10;
+const cantidadNumeros: number = 100;
 const resultado = generarFibonacci(cantidadNumeros);
 
 // Muestra el resultado en la consola
-console.log(`Los numeros ${cantidadNumeros} números de Fibonacci son:`);
+console.log(`Los primeros ${cantidadNumeros} números de Fibonacci son:`);
 console.log(resultado);
 console.log('este resultado es de prueba');
