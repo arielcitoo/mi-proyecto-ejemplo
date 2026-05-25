300ae548d1589ec3b99e5479cb1dca7c6d30f64a
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-05-25 10:43:14 -0400

    refactorizacion fibonacci

--- component.ts
+++ component.ts
@@ -6,15 +6,14 @@ function generarFibonacci(n: number): number[] {
     if (n === 1) return [0];
 
     for (let i = 2; i < n; i++) {
-        const siguienteNumero = secuencia[i - 1] + secuencia[i - 2];
-        secuencia.push(siguienteNumero);
+        secuencia.push(secuencia[i - 1] + secuencia[i - 2]);
     }
 
     return secuencia;
 }
 
 // Configura cuántos números quieres mostrar
-const cantidadNumeros: number = 10;
+const cantidadNumeros: number = 100;
 const resultado = generarFibonacci(cantidadNumeros);
 
 // Muestra el resultado en la consola
