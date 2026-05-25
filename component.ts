0147482f9bbe0ca90dd819fd3ce2de8fe5b035cc
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-05-25 09:30:46 -0400

    este resultado es de brueba

--- component.ts
+++ component.ts
@@ -20,3 +20,4 @@ const resultado = generarFibonacci(cantidadNumeros);
 // Muestra el resultado en la consola
 console.log(`Los primeros ${cantidadNumeros} números de Fibonacci son:`);
 console.log(resultado);
+console.log('este resultado es de prueba');
