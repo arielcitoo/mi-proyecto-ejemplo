5a1237b08956ef5c35e18ad1e2b8f8bee917de3e
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-05-28 14:08:25 -0400

    la ch

--- component.ts
+++ component.ts
@@ -19,4 +19,4 @@ const resultado = generarFibonacci(cantidadNumeros);
 // Muestra el resultado en la consola
 console.log(`Los primeros ${cantidadNumeros} números de Fibonacci son:`);
 console.log(resultado);
-console.log('este resultado es de prueba');
+console.log('este resultado de la concha de tu hermana');
