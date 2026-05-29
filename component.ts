10b06846af3f477dfc231e1e60a5f94ff7ef4fbf
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-05-29 15:59:50 -0400

    git paht

--- component.ts
+++ component.ts
@@ -1,6 +1,6 @@
 // Función que genera los primeros 'n' números de Fibonacci
 function generarFibonacci(n: number): number[] {
-    const secuencia1: number[] = [0, 1];
+    const secuencia: number[] = [0, 1];
 
     if (n <= 0) return [];
     if (n === 1) return [0];
@@ -19,4 +19,4 @@ const resultado = generarFibonacci(cantidadNumeros);
 // Muestra el resultado en la consola
 console.log(`Los primeros ${cantidadNumeros} números de Fibonacci a contuanción:`);
 console.log(resultado);
-console.log('este es un resultado nuevo');
+console.log('resultado finallitimo');
