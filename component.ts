d5e9c1d2f3900fc30cfb665914d1e2e87fe72bc4
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-06-01 12:11:10 -0400

    cambio inicial

--- component.ts
+++ component.ts
@@ -1,22 +1,9 @@
-// Optimizar codigo Fibonacci
-function generarFibonacci(n: number): number[] {
-    const secuencia: number[] = [0, 1];
-
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-
-    for (let i = 2; i < n; i++) {
-        secuencia.push(secuencia[i - 1] + secuencia[i - 2]);
+// fibonacci.ts
+function fibonacci(n: number): number {
+    if (n <= 1) {
+        return n;
     }
-
-    return secuencia;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-// Configura cuántos números quieres mostrar
-const cantidadNumeros: number = 20;
-const resultado = generarFibonacci(cantidadNumeros);
-
-// Muestra el resultado en la consola
-console.log(`Los primeros ${cantidadNumeros} números de Fibonacci a contuanción:`);
-console.log(resultado);
-
+console.log(fibonacci(10)); // 55
