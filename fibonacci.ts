4983772b50fefd69057249cff895f24dc27f5559
arielcitoo <145722031+arielcitoo@users.noreply.github.com>
2026-06-02 15:16:48 -0400

    arieiei

--- fibonacci.ts
+++ fibonacci.ts
@@ -10,4 +10,4 @@ function fibonacci(n: number): number {
 // Ejecutar ejemplo
 const resultado = fibonacci(10);
 console.log(`Resultado: ${resultado}`);
-console.log('hola mundo');
+console.log('hola mundo epo');
